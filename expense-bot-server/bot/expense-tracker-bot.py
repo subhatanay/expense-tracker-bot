82527b15d226033d9dcfbdc2473b942e40a60afb
subhatanay <itsme.subhajitghosh56@gmail.com>
2024-09-27 20:02:43 +0530

    Update expense-tracker-bot.py

--- expense-bot-server/bot/expense-tracker-bot.py
+++ expense-bot-server/bot/expense-tracker-bot.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask, jsonify, request
 from flask_cors import CORS,cross_origin
 
-genai.configure(api_key="AIzaSyC1kY8QZs1J83USpS6CmAUeGyTeHVcyLrU")
+genai.configure(api_key="")
 
 
 def start_chat_session(): 
